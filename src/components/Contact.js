9e284e6a96eb78601c6f8910a7b155356305aec3
ddorest <ddodivw@gmail.com>
2022-06-26 23:37:39 +0700

    update App.js, Contact.js, and Header.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,21 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
+import './Contact.css';
+
+const Contact = (props) => {
+const {name, photo, phone, email} = props.data;
     return (
-        <></>
+        <div className='contact-card'>
+            <div className='img-card'> 
+                <img src={photo} alt="photo" />
+            </div>
+            <div className='detail-contact'>
+                <h3>{name} </h3>
+                <p> {phone} </p>
+                <p> {email} </p>
+            </div>
+        </div>
     )
 }
-
 export default Contact;
